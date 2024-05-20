d7ba32d177402a2df3a688893d0a88f0a4cc4e5f
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 16:33:42 +0200

    simplify product load

--- src/routes/product/[id]/+page.js
+++ src/routes/product/[id]/+page.js
@@ -1,14 +1,6 @@
-export async function load({ fetch, params, data }) {
+export async function load({ fetch, params }) {
   const { id } = params;
   const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-  const productClient = await response.json();
-  const { productServer } = data;
-  return {
-    title: productServer.title,
-    description: productServer.description,
-    category: productServer.category,
-    image: productServer.image,
-    id: productServer.id,
-    price: productClient.price,
-  };
+  const product = await response.json();
+  return { product };
 }
--- src/routes/product/[id]/+page.server.js
+++
@@ -1,8 +0,0 @@
-export async function load({ fetch, params }) {
-  const { id } = params;
-  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-  const productServer = await response.json();
-  return {
-    productServer
-  };
-}
