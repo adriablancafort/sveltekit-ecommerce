081224ec8f75c2d1a0eeec585ff7c0eb5b92b6c1
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 16:24:36 +0200

    moved add to cart to store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,2 +1,11 @@
 import { writable } from 'svelte/store';
+
+// Cart
+
 export const cart = writable([]);
+
+export function addToCart() {
+    cart.update((items) => {
+        return [...items, product];
+    });
+}
