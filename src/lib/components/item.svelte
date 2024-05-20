00354ee4f3f45ffa58dae74c56988b72e7ada32f
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 20:10:15 +0200

    link

--- src/lib/components/item.svelte
+++ src/lib/components/item.svelte
@@ -2,10 +2,10 @@
   export let product;
 </script>
 
-<div>
-<img src={product.image} alt={product.title} />
-<h3>{product.title}</h3>
+<a href="/product/{product.id}">
+  <img src={product.image} alt={product.title} />
+  <h3>{product.title}</h3>
   <p>Price: {product.price}</p>
   <p>Category: {product.category}</p>
   <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
-</div>
+</a>
