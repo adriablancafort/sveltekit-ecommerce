9176bddc33c97cc030200a9b251a9691604480b9
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 17:06:28 +0200

    header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,3 @@
+<header class="h-20 px-6 py-4">
+    <img src="/asilor-logo-dark.svg" alt="Logo" height="210" width="297">
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import "../app.css";
   import Cart from "$lib/components/cart.svelte";
+  import Header from "$lib/components/header.svelte";
   import { onNavigate } from "$app/navigation";
 
   onNavigate((navigation) => {
@@ -19,6 +20,7 @@
   <div
     class="mx-auto max-w-[1400px] group-data-[state=open]:pr-52 duration-300 ease-in-out"
   >
+    <Header />
     <slot />
   </div>
   <Cart />
