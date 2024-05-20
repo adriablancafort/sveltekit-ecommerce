8736a4f66dccb6ea8602f3289bda7a24e036b81d
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 16:37:41 +0200

    remove cart open effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script>
   import "../app.css";
-  import Cart from "./cart.svelte";
+  import Cart from "$lib/components/cart.svelte";
   import { onNavigate } from "$app/navigation";
-  import { onMount } from "svelte";
 
   onNavigate((navigation) => {
     if (!document.startViewTransition) return;
@@ -14,17 +13,9 @@
       });
     });
   });
-
-  let mainElement;
-
-  onMount(() => {
-    setTimeout(() => {
-      mainElement.dataset.state = "open";
-    }, 2000);
-  });
 </script>
 
-<main bind:this={mainElement} class="overflow-hidden group" data-state="closed">
+<main class="overflow-hidden group" data-state="open">
   <div
     class="mx-auto max-w-[1400px] group-data-[state=open]:pr-52 duration-300 ease-in-out"
   >
