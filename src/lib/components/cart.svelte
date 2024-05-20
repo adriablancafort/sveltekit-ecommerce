9ee7af6d9640d04c517f7d3523127dfa356d5bdd
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 20:09:57 +0200

    format

--- src/lib/components/cart.svelte
+++ src/lib/components/cart.svelte
@@ -3,9 +3,7 @@
   import { cart } from "$lib/store.js";
 </script>
 
-<div
-  class="cart fixed top-0 right-0 w-52 translate-x-full group-data-[state=open]:translate-x-0 duration-300 ease-in-out"
->
+<div class="cart fixed top-0 right-0 w-52 translate-x-full group-data-[state=open]:translate-x-0 duration-300 ease-in-out">
   <h2 class="text-2xl">Cart</h2>
   {#each $cart as product (product.id)}
     <Item {product} />
