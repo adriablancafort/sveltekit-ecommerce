4f190c47f642d8ed674e21b36c299b2bca4b6188
Adria Blancafort <adriablancafort@gmail.com>
2024-05-20 20:10:04 +0200

    link

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,3 +1,5 @@
 <header class="h-20 px-6 py-4">
-    <img src="/asilor-logo-dark.svg" alt="Logo" height="210" width="297">
+    <a href="/">
+        <img src="/asilor-logo-dark.svg" alt="Logo" height="210" width="297">
+    </a>
 </header>
