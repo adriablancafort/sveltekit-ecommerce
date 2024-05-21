f275038132f15bbaaf966a870d08b36101b4d585
Adria Blancafort <adriablancafort@gmail.com>
2024-05-21 08:50:47 +0200

    search filter

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,38 +1,33 @@
 <script>
-    let searchTerm = '';
-    let searchResults = [];
+    let input = '';
+    let results = [];
 
-    async function search() {
-        if (!searchTerm.trim()) {
-            searchResults = [];
-            return;
-        }
-
-        const response = await fetch('https://fakestoreapi.com/products');
+    const search = async () => {
+        const response = await fetch('https://fakestoreapi.com/products?limit=10');
         const products = await response.json();
-        searchResults = products.filter(product =>
-            product.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
+        if (input !== '') {
+            results = products.filter(product => product.title.toLowerCase().includes(input.toLowerCase()));
+        } else {
+            results = [];
+        }
     }
 
-    $: searchTerm && search();
+    $: input, search();
 </script>
 
 <div class="w-full max-w-xs mx-auto bg-white rounded relative">
     <input
-        class="border-none appearance-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class="border-none outline-none shadow-outline w-full py-2 px-3"
         type="text"
         placeholder="Search"
-        bind:value={searchTerm}
+        bind:value={input}
     />
 
-    {#if searchTerm}
-        <ul class="-mt-0.5 absolute bg-white w-full z-10">
-            {#each searchResults as result (result.id)}
-                <a href="/product/{result.id}">
-                    <li class="border-t py-2">{result.title}</li>
-                </a>
-            {/each}
-        </ul>
-    {/if}
+    <ul class="absolute w-full bg-white z-10 rounded-b pt-1 -mt-1">
+        {#each results as result (result.id)}
+            <a href="/product/{result.id}">
+                <li class="border-t py-2">{result.title}</li>
+            </a>
+        {/each}
+    </ul>
 </div>
