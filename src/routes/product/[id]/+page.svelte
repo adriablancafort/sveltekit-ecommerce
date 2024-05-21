a6486d18c84e2877489f00d21c2ba8fe9b106597
Adria Blancafort <adriablancafort@gmail.com>
2024-05-21 09:33:32 +0200

    product page

--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -5,16 +5,34 @@
 </script>
 
 <svelte:head>
-  <title>Asilor - {product.title}</title>
+  <title>{product.title} - Asilor</title>
 </svelte:head>
 
-<div>
-  <img class="w-80" src={product.image} alt={product.title} style:--image="image-{product.id}" />
-  <h3 class="text-2xl">{product.title}</h3>
-  <p>{product.description}</p>
-  <p>Price: {product.price}</p>
-  <p>Category: {product.category}</p>
-  <button class="text-white bg-red-600 hover:bg-red-500 rounded text-lg px-4" on:click={() => addToCart(product)}>Add to cart</button>
+<nav aria-label="Breadcrumb">
+    <ol class="flex space-x-2 py-6">
+        <li><a href="/" class="hover:underline">Asilor</a></li>
+        <span>/</span>
+        <li><a href="/{product.category}" class="hover:underline">{product.category}</a></li>
+        <span>/</span>
+        <li><a href="/product/{product.id}">{product.title}</a></li>
+    </ol>
+</nav>
+
+<div class="grid grid-cols-2 space-x-12">
+    <img src={product.image} alt={product.title} class="rounded-lg border mb-8" style:--image="image-{product.id}"/>
+
+    <div class="space-y-4">
+        <h1 class="text-4xl">{product.title}</h1>
+
+        <button class="bg-red-600 hover:bg-red-500 text-white rounded px-8 py-1 text-lg" on:click={() => addToCart(product)}>Add to cart</button>
+
+        <div>
+          <p>Price: {product.price}</p>
+          <p>Category: {product.category}</p>
+          <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+          <p>Description: {product.description}</p>
+        </div>
+    </div>
 </div>
 
 <style>
