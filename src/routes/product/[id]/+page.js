e2d03071b4070983517e5b24274552d866949dbd
Adria Blancafort <adriablancafort@gmail.com>
2024-05-21 13:27:41 +0200

    dummy json

--- src/routes/product/[id]/+page.js
+++ src/routes/product/[id]/+page.js
@@ -1,6 +1,6 @@
 export async function load({ fetch, params }) {
   const { id } = params;
-  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
   const product = await response.json();
   return { product };
 }
