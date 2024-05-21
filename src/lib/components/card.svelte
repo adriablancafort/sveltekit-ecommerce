a7332bac50032cf6fdfc75f668c6a5d3ff718f48
Adria Blancafort <adriablancafort@gmail.com>
2024-05-21 13:26:39 +0200

    dummy json

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -6,7 +6,7 @@
 <div class="border border-gray-300 rounded p-5 hover:scale-105 hover:shadow-xl">
   <a href="/product/{product.id}">
     <img
-      src={product.image}
+      src={product.thumbnail}
       alt={product.title}
       style:--image="image-{product.id}"
     />
@@ -14,7 +14,6 @@
   <h3 class="text-2xl">{product.title}</h3>
   <p>Price: {product.price}</p>
   <p>Category: {product.category}</p>
-  <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
   <button class="text-white bg-red-600 hover:bg-red-500 rounded text-lg px-4" on:click={() => addToCart(product)}>Add to cart</button>
 </div>
 
--- src/lib/components/item.svelte
+++ src/lib/components/item.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a href="/product/{product.id}">
-  <img src={product.image} alt={product.title} />
+  <img src={product.thumbnail} alt={product.title} />
   <h3>{product.title}</h3>
   <p>Price: {product.price}</p>
   <p>Category: {product.category}</p>
